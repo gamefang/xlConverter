abdd1b6589e7634f85f986089b62cd197c032bb5
zhang <337503110@qq.com>
2018-08-30 23:20:51 +0800

    添加全字符串数组类型，类型前缀可配置。

--- xl2json/xl2json.py
+++ xl2json/xl2json.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-#2018/8/24 实现excel策划配置文件批量导出为json。
 #不能完美支持python2。
+#2018/8/24 实现excel策划配置文件批量导出为json。
+#2018/8/30 添加全字符串数组类型，类型前缀可配置。
 
 CONF_FILE='xl2json_conf.json'   #配置文件路径
 
 __doc__='''
 配置文件说明(%s)
     配置文件可调整部分功能，需要严格遵循json格式。
-    @xl_dir: Excel文件的存放目录，会自动搜寻目录下所有Excel文件。
+    @xl_dir: Excel文件的存放目录，会自动搜寻目录下所有Excel文件‍（递归式）。
     @json_dir: 输出json文件的目录。
     @output_in_one: 如果为false或0，则分别输出json文件，文件名为Excel对应sheet名；
                     如果为字符串，则输出至一个文件，例如填写config.json，则将所有Excel文件内容输出至该文件中。
@@ -15,21 +16,22 @@ __doc__='''
     @sheet_name_prefix: Excel文件中，加此前缀的sheet会被导出，可支持多个。前缀只允许一个字符。
     @bound_tag: Excel一个sheet中配置的边界符号，只有边界内的配置内容才会被导出。
     @note_signs: Excel配置中单行、单列注释的前缀，被注释则不会被导出。
+    @var_type_pre: Excel配置中字段名前缀字符串，用以区分数据类型，需严格按照以下顺序：
+        0-python的int，对应json中的数字num；
+        1-python的float，对应json中的数字num；
+        2-python的string，对应json中的字符串string；
+        3-借用python的tuple，对应json中的全字符串数组（自动补全双引号）;
+        4-python的list，对应json中的普通数组array（全数字或数串混合自补双引号）；
+        5-python的dict，对应json中的对象object（自补双引号）；
+        6-python的boolean，对应json中的布尔值bool；
+        暂不包括json中的null类型。
     @keep_var_type: 是否在json的key中保留字段名前缀。
     @read_me_mode: 是否查看说明。
 Excel文件格式说明
     1.[工作簿]需要导出的所有Excel文件必须放在配置文件夹中。
     2.[工作表]需要导出的sheet页前需加前缀，如分开导出则json文件名为前缀後的sheet名。
     3.[导出区域]需要导出的区域要用边界符号框住，位置为右上和左下。
-    4.[字段名称]右上边界符号所在行必须为表头，第一个字符表示字段的数据类型，需遵循以下规则：
-        i-python的int，对应json中的数字num；
-        f-python的float，对应json中的数字num；
-        s-python的string，对应json中的字符串string；
-        t-python的tuple，json无需使用。
-        l-python的list，对应json中的数组array；
-        d-python的dict，对应json中的对象object；
-        b-python的boolean，对应json中的布尔值bool；
-        暂不包括json中的null类型。
+    4.[字段名称]右上边界符号所在行必须为表头，第一个字符表示字段的数据类型，详见@var_type_pre说明。
     5.[数据规范]同列Excel数据需要遵循字段的数据类型，否则会产生错误。
         数字：Excel所见即所得；
         字符串：无需添加两侧""；
@@ -113,8 +115,8 @@ def worksheet_handle(sheet,cfg):
                 else:
                     raise Exception("<FILE>%s <SHEET>%s <ROW>%s <COLUMN>%s:Note Error!" % (FILENAME,SHEETNAME,ROW+1,COLUMN+1) )
             else:
-                if r==bounds[1][0]: #第一行存储类型信息（字符串首字符）
-                    types[c]=v[0]
+                if r==bounds[1][0]: #第一行存储类型信息
+                    types[c]=get_type_pre(v,cfg)
                     cv=v
                 else:
                     cv=clean_cell_data(sheet.cell(r,c),types[c])    #Cell对象数据清洗并保存为实际类型
@@ -143,74 +145,82 @@ def get_sheet_bounds(sheet,cfg):
         raise Exception("<FILE>%s <SHEET>%s:bound tags error!\nNeed exactly 2 tags,and can't be in the same row or column!" % (FILENAME,SHEETNAME))
     else:   #标记正常，开始遍历取值
         return bounds
-    
-def clean_cell_data(cell,typ):
+        
+def get_type_pre(val,cfg):
+    '''
+    获取表头字段中含有的数据类型信息。
+    @param val: 字段名字符串。
+    @param cfg: 全局配置对象。
+    @return: 数值型类型值：
+        0-int
+        1-float
+        2-string
+        3-tuple(string array in json)
+        4-list
+        5-dict
+        6-boolean
+    '''
+    if val.lower()=='key':return 2    #json的key必须是string
+    for i,pre in enumerate(cfg['var_type_pre']):
+        if val.startswith(pre):return i
+    return 2    #特殊情况均视为str
+
+def clean_cell_data(cell,type_value):
     '''
     Get a python type variable,from Excel-Cell-Object in xlrd.
     Need RE.
-    #cell:cell object in xlrd.
-    #typ:
-        i-int
-        f-float
-        s-string
-        t-tuple
-        l-list
-        d-dict
-        b-boolean
+    @param cell: cell object in xlrd.
+    @param type_value: the value of type:
+        0-int
+        1-float
+        2-string
+        3-tuple(string array in json)
+        4-list
+        5-dict
+        6-boolean
+    @return: exact type of data.
     '''
     t=cell.ctype
     if t==0 or not cell.value:    #空，或Excel中残留的'型单元格等情况
-        if typ=='i':
-            return 0
-        elif typ=='f':
-            return 0.0
-        elif typ=='s':
-            return ""
-        elif typ=='t':
-            return ()
-        elif typ=='l':
-            return []
-        elif typ=='d':
-            return {}
-        elif typ=='b':
-            return False
+        return {
+            0:0,    #int
+            1:0.0,  #float
+            2:'',   #str
+            3:[],   #string array
+            4:[],   #normal array
+            5:{},   #object
+            6:False,    #bool
+            }.get(type_value,str(cell.value))
     elif t==1:  #非数字
         v=str(cell.value)
-        if typ=='i' and re.match('^(\-|\+)?\d$',v):    #文本形式保存的整数
+        if type_value==0 and re.match('^(\-|\+)?\d$',v):    #文本形式保存的整数
             return int(v)
-        elif typ=='f' and re.match('^(-?\d+)(\.\d+)?$',v): #文本形式保存的符点数
+        elif type_value==1 and re.match('^(-?\d+)(\.\d+)?$',v): #文本形式保存的符点数
             return float(v)
-        elif typ=='s':
-            return v
-        elif typ=='t':
-            return eval('(%s,)'%v)
-        elif typ=='l':
-            return eval('[%s,]'%v)
-        elif typ=='d':
-            return eval('{%s,}'%v)
-        elif typ=='b':
-            return (v in ('True','TRUE','true','t','T','1'))    #可以当True用的字符串
+        elif type_value==2:
+            return v    #str
+        elif type_value==3:
+            return v.split(',')  #string array
+        elif type_value==4:
+            return eval('[%s,]'%v)   #normal array
+        elif type_value==5:
+            return eval('{%s,}'%v)  #object
+        elif type_value==6:
+            return (v in ('True','TRUE','true','t','T','1')) #bool,可以当True用的字符串
     elif t==2:  #数字
-        if typ=='i':
-            return int(cell.value)
-        elif typ=='f':
-            return float(cell.value)
-        elif typ=='s':
-            return str(int(cell.value))  #整数字符串（通常为设计问题）
-        elif typ=='t':
-            return eval( '(%s,)' % int(cell.value) )    #容器里面暂时只存整数
-        elif typ=='l':
-            return eval( '[%s,]' % int(cell.value) )    #容器里面暂时只存整数
-        elif typ=='d':
-            return eval( '{%s,}' % int(cell.value) )    #容器里面暂时只存整数
-        elif typ=='b':
-            return bool(cell.value)  #有值就算True
+        if type_value==3:
+            return v.split(',')
+        return {
+            0:int(cell.value),  #int
+            1:float(cell.value),    #float
+            2:str(int(cell.value)), #str,整数字符串（通常为设计问题）
+            4:eval( '[%s,]' % int(cell.value) ), #normal array,容器里面暂时只存整数
+            5:eval( '{%s,}' % int(cell.value) ),    #object,容器里面暂时只存整数
+            6:bool(cell.value), #bool,有值就算True
+        }.get(type_value,str(cell.value))
     elif t==4:  #Excel中的TRUE和FALSE
-        if typ=='b':
-            if cell.value:  #TRUE实际等于1.0
-                return True
-            else:
-                return False
+        if type_value==6:
+            return bool(cell.value)  #TRUE实际等于1.0
         else:
             print('###WARNING!NOT A BOOL! cell.value:{self.value},cell.ctype:{self.ctype}'.format(self=cell))
             return str(cell.value)
@@ -218,7 +228,7 @@ def clean_cell_data(cell,typ):
         print('###WARNING!ERROR! cell.value:{self.value},cell.ctype:{self.ctype}'.format(self=cell))
         return str(cell.value)
     
-def dic_convert(oridata,keep_var_type=False):
+def dic_convert(oridata,cfg):
     '''
     From:
     [['sId', 'sName', 'sTip', 'iFulfill_value'],
@@ -228,7 +238,7 @@ def dic_convert(oridata,keep_var_type=False):
     {'test': {'Name': '成就', 'Tip': '一个测试成就', 'Fulfill_value': 100},
      'testhide': {'Name': '隐藏成就', 'Tip': '又一个测试成就', 'Fulfill_value': 100}}
     @param oridata: original 2-dimensional list.
-    @param keep_var_type: whether keep the variable type prefix in dic keys.
+    @param cfg: global config object.
     @return: a copy of new styled dic.
     '''
     listdata=oridata.copy()
@@ -237,10 +247,11 @@ def dic_convert(oridata,keep_var_type=False):
     for item in listdata:
         subdic={}
         for i in range(1,len(item)):
-            if keep_var_type:
+            if cfg['keep_var_type']:
                 subdic[ pro_names[i] ]=item[i]
             else:
-                subdic[ pro_names[i][1:] ]=item[i]
+                pre_len=len(cfg['var_type_pre'][get_type_pre(pro_names[i],cfg)])
+                subdic[ pro_names[i][pre_len:] ]=item[i]
         dic[ item[0] ]=subdic
     return dic
 
@@ -280,7 +291,7 @@ def main():
         raw_data.update(workbook_handle(fn,cfg))
     #原始数据转化
     data={
-        k[1:]:dic_convert(v,cfg['keep_var_type'])
+        k[1:]:dic_convert(v,cfg)
         for k,v in raw_data.items()
         }
     #输出为json
